72163b6e29e8101c77c70fce67258eb5f419f444
Tokru-0204 <doanthanhhao0204@gmail.com>
2024-05-23 10:43:08 +0700

    upload1

--- src/main/java/com/example/githubactioncicd/GitHubActionCICDApplication.java
+++ src/main/java/com/example/githubactioncicd/GitHubActionCICDApplication.java
@@ -8,7 +8,5 @@ public class GitHubActionCICDApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(GitHubActionCICDApplication.class, args);
-        EEÊRR
-        âssa
-
+    }
 }
